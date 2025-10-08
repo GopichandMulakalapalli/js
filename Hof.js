257aba7fc688f150186f5756ed7112864d99e6b9
Gopichand <130022786+GopichandMulakalapalli@users.noreply.github.com>
2025-10-08 18:43:50 +0530

    Add files via upload

---
+++ Hof.js
@@ -0,0 +1,9 @@
+function myButton(x,y){
+    console.log(x,y);
+}
+
+
+
+myButton(100,200);
+
+myButton("Sagar","kabir");
---
+++ object.js
@@ -0,0 +1,9 @@
+var user = {};
+//Literal Notation 
+
+class Emp {
+    empname ="Vikram";
+}
+
+var emp1 = new Emp();
+//Constructor

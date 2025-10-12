8658db98fb85d1054e2993b5b153061bcfa97756
Gopichand <130022786+GopichandMulakalapalli@users.noreply.github.com>
2025-10-12 23:44:48 +0530

    Add files via upload

---
+++ spread.js
@@ -0,0 +1,18 @@
+var obj1 = {
+    name:"d1",
+    city:"c1",
+    gender:"g1",
+};
+
+var obj2 = {
+    ...obj1,
+    pin : 99999,
+};
+
+
+var arr1 =[10,20,30];
+var arr2 = [...arr1];
+
+console.log(arr1);
+console.log("================");
+console.log(arr2);

ad589a4b261369f53a7412f55c4618852dd745d8
Gopichand <130022786+GopichandMulakalapalli@users.noreply.github.com>
2025-10-14 11:15:38 +0530

    Add files via upload

---
+++ String.js
@@ -0,0 +1,34 @@
+var data = "male.dfdshkfoi;dshofvo";
+
+var content2 = `kfdggbzdfiugjbfgf9ougiusi/${data}`;
+
+//console.log(content2);
+
+
+var content = "  Hello EveryOne    ";
+
+var newContent = " From Delhi";
+
+//console.log(content.toLowerCase());
+
+//console.log(content.toUpperCase());
+
+//console.log(content.length);
+
+//console.log(content.substring(0,4));
+
+//console.log(content.substr(0,3));
+
+//console.log(content.replace("EveryOne","kabir"));
+
+//console.log(content.concat(newContent));
+
+
+//console.log(content.trim());
+
+console.log(content.trimStart());
+
+
+
+console.log(content.trimEnd());
+
